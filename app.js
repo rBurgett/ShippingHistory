8b6c7342d005f56612db619191b8615aa0e7a66e
Ryan <ryan@burgettweb.net>
2015-02-27 18:48:09 -0500

    Configured Mongo/Mongoose shipment collection, added GET route

--- app.js
+++ app.js
@@ -53,66 +53,57 @@ var Shipment = mongoose.model('Shipment', shipmentSchema);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
- //   'use strict';
- /*   var shipmentSchema = mongoose.Schema({
-        trackingNumber: Number,
-        shipDate: String,
-        deliveryDate: Number,
-        recipientID: String,
-        contactName: String,
-        companyName: String,
-        address1: String,
-        address2: String,
-        city: String,
-        state: String,
-        zip: Number,
-        phone: String,
-        recEmail: String,
-        recEmailS: String,
-        recEmailT: String,
-        recEmailE: String,
-        recEmailD: String,
-        otherEmail1: String,
-        otherEmail1S: String,
-        otherEmail1T: String,
-        otherEmail1E: String,
-        otherEmail1D: String,
-        otherEmail2: String,
-        otherEmail2S: String,
-        otherEmail2T: String,
-        otherEmail2E: String,
-        otherEmail2D: String,
-        weight: Number,
-        serviceType: Number,
-        l: Number,
-        w: Number,
-        h: Number,
-        declaredValue: Number,
-        netCost: Number,
-        poNumber: String,
-        customerReference: String
-    });
-    var Shipment = mongoose.model('Shipment', shipmentSchema);*/
-    console.log('Connected!');
+     console.log('Connected!');
 });
 
-/**********************************************************************/
+/*************************Express Server************************/
 app.engine('hbs', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-fs.readFile('./Shipping_History.csv', 'utf-8', function(err, data) {
+app.get('/shipments', function(req, res) {
     'use strict';
-    if (err) {throw err;}
-    csv.parse(data, {columns: true}, function(err, shipmentsArray){
-//        console.log(shipmentsArray);
-    });
+    if(req.query.q) {
+        Shipment.find().limit(req.query.q).exec(function(err, shipments) {
+            if(err) {res.send(err);}
+            res.send(shipments);
+        });
+    } else {
+        Shipment.find(function(err, shipments) {
+            if(err) {res.send(err);}
+            res.send(shipments);
+        });
+    }
 });
 
-/*var server = app.listen(3000, function() {
+var server = app.listen(3000, function() {
+    'use strict';
     var host = server.address().address;
     var port = server.address().port;
     console.log('App listening at http://localhost:%s', port);
+});
+
+/*********************CSV File Reader*******************************/
+/*fs.readFile('./Shipping_History.csv', 'utf-8', function(err, data) {
+    'use strict';
+    if (err) {throw err;}
+    csv.parse(data, {columns: true}, function(err, shipments){
+        var afterImport = _.after(shipments.length, function() {
+            Shipment.find(function(err, shipments) {
+                if(err) {console.error(err)}
+//                console.log(shipments);
+            });
+        });
+        for (var i = 0; i < shipments.length; i++) {
+            var shipment = new Shipment(shipments[i]);
+            shipment.save(function(err) {
+                if(err) {console.log(err);}
+                else {
+                    afterImport();
+                }
+           });
+        }
+    });
 });*/
