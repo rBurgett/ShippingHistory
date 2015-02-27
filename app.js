58a3d80ccd0f224947febb63794e20b9a2c181a9
Ryan <ryan@burgettweb.net>
2015-02-27 12:08:19 -0500

    Added filesystem & CSV parsing

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+/*global require, console*/
+
 var express = require('express'),
     exphbs = require('express-handlebars'),
+    fs = require('fs'),
+    csv = require('csv'),
+    _ = require('lodash'),
     app = express();
 
 app.engine('hbs', exphbs({defaultLayout: 'main'}));
@@ -8,12 +13,16 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
-//app.get('/', function(req, res) {
-//    res.render('home');
-//});
+fs.readFile('./Shipping_History.csv', 'utf-8', function(err, data) {
+    'use strict';
+    if (err) {throw err;}
+    csv.parse(data, {columns: true}, function(err, shipmentsArray){
+        console.log(shipmentsArray);
+    });
+});
 
-var server = app.listen(3000, function() {
+/*var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Example app listening at http://localhost:%s', port)
-});
+    console.log('App listening at http://localhost:%s', port);
+});*/
